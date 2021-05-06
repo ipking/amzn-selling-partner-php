689338ac535e547fb4d9659865445e607b1ccc7e
ipking <667997@gmail.com>
2021-05-06 18:12:12 +0800

    add endpoint

---
+++ src/Core/Country.php
@@ -0,0 +1,75 @@
+<?php
+namespace SellingPartner\Core;
+
+class Country{
+	public static $marketplaceId = [
+		'CA' => 'A2EUQ1WTGCTBG2',
+		'US' => 'ATVPDKIKX0DER',
+		'MX' => 'A1AM78C64UM0Y8',
+		'BR' => 'A2Q3Y263D00KWC',
+		
+		'ES' => 'A1RKKUPIHCS9HS',
+		'GB' => 'A1F83G8C2ARO7P',
+		'FR' => 'A13V1IB3VIYZZH',
+		'NL' => 'A1805IZSGTT6HS',
+		'DE' => 'A1PA6795UKMFR9',
+		'IT' => 'APJ6JRA9NG5V4',
+		'SE' => 'A2NODRKZP88ZB9',
+		'PL' => 'A1C3SOZRARQ6R3',
+		'TR' => 'A33AVAJ2PDY3EV',
+		'AE' => 'A2VIGQ35RCS4UG',
+		'IN' => 'A21TJRUUN4KGV',
+		
+		'SG' => 'A19VAU5U5O7RUS',
+		'AU' => 'A39IBJ37TRP1C6',
+		'JP' => 'A1VC38T7YXB528',
+	];
+	
+	public static $endpoint = [
+		'CA' => 'https://sellingpartnerapi-na.amazon.com',
+		'US' => 'https://sellingpartnerapi-na.amazon.com',
+		'MX' => 'https://sellingpartnerapi-na.amazon.com',
+		'BR' => 'https://sellingpartnerapi-na.amazon.com',
+		
+		'ES' => 'https://sellingpartnerapi-eu.amazon.com',
+		'GB' => 'https://sellingpartnerapi-eu.amazon.com',
+		'FR' => 'https://sellingpartnerapi-eu.amazon.com',
+		'NL' => 'https://sellingpartnerapi-eu.amazon.com',
+		'DE' => 'https://sellingpartnerapi-eu.amazon.com',
+		'IT' => 'https://sellingpartnerapi-eu.amazon.com',
+		'SE' => 'https://sellingpartnerapi-eu.amazon.com',
+		'PL' => 'https://sellingpartnerapi-eu.amazon.com',
+		'TR' => 'https://sellingpartnerapi-eu.amazon.com',
+		'AE' => 'https://sellingpartnerapi-eu.amazon.com',
+		'IN' => 'https://sellingpartnerapi-eu.amazon.com',
+		
+		'SG' => 'https://sellingpartnerapi-fe.amazon.com',
+		'AU' => 'https://sellingpartnerapi-fe.amazon.com',
+		'JP' => 'https://sellingpartnerapi-fe.amazon.com',
+	];
+	
+	public static $region = [
+		'CA' => 'us-east-1',
+		'US' => 'us-east-1',
+		'MX' => 'us-east-1',
+		'BR' => 'us-east-1',
+		
+		'ES' => 'eu-west-1',
+		'GB' => 'eu-west-1',
+		'FR' => 'eu-west-1',
+		'NL' => 'eu-west-1',
+		'DE' => 'eu-west-1',
+		'IT' => 'eu-west-1',
+		'SE' => 'eu-west-1',
+		'PL' => 'eu-west-1',
+		'TR' => 'eu-west-1',
+		'AE' => 'eu-west-1',
+		'IN' => 'eu-west-1',
+		
+		'SG' => 'us-west-2',
+		'AU' => 'us-west-2',
+		'JP' => 'us-west-2',
+	
+	];
+}
+
---
+++ src/Core/Endpoint.php
@@ -0,0 +1,8 @@
+<?php
+namespace SellingPartner\Core;
+
+class Endpoint{
+	public static $EUROPE = 'https://sellingpartnerapi-eu.amazon.com';
+	public static $FAR_EAST = 'https://sellingpartnerapi-fe.amazon.com';
+	public static $NORTH_AMERICA = 'https://sellingpartnerapi-na.amazon.com';
+}
